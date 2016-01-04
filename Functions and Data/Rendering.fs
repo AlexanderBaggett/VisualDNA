b19c0781bef3ecf1772ede10143c814c4e746307
Alexander Baggett <alexander.baggett@gmail.com>
2016-01-04 00:46:36 -0600

    Built basic framework for type system

---
+++ Functions and Data/Rendering.fs
@@ -0,0 +1,36 @@
+﻿
+namespace Functions_and_Data
+open System.Drawing
+open Data
+module Rendering =
+
+// This type system may need reworking, but there is a ton to implement.
+    type Renderer =
+                {
+                    Name : string
+                    DisplayName : string
+                    Resolution : int * int
+                    NodeApply : Node -> Renderer
+                    Apply : Renderer -> Renderer
+
+
+                
+                }
+    type SVGRenderer = 
+        {
+           Renderer : Renderer
+           SimpleRender : Node -> string
+           FinalPass : Renderer -> string
+
+        }
+
+    type RasterRenderer = 
+        {   
+
+            Renderer : Renderer
+            SimpleRender : Node -> Bitmap
+            FinalPass : Renderer -> Bitmap
+
+        }
+
+
---
+++ Functions and Data/fundata.fs
@@ -0,0 +1,55 @@
+﻿namespace Functions_and_Data
+open System.Drawing
+
+module Data =
+    type VectorImage = class end
+    type SliderDisplayType = 
+                             |Slider 
+                             |TextBox
+    type SliderOption = { Name: string
+                          Current : float
+                          Min : float
+                          Max : float
+                          DisplayType : SliderDisplayType
+                          Adjust : float -> SliderOption
+                
+                          }
+
+
+    type CheckedOption = { Name: string
+                           Checked : bool
+                           Adjust : bool -> CheckedOption
+                          
+                          }
+    type RadioOption = {   Name: string
+                           Options : (string * bool) list
+                           Adjust : string * bool-> RadioOption
+                          
+                          }
+    type Node = 
+    
+        {
+          Name :string
+          DisplayName : string
+          ParentNodes : Node list
+          ChildNodes : Node list
+          Vector2D :  VectorImage
+          SliderOptions : SliderOption list
+          CheckedOptions : CheckedOption list
+          RadioOptions : RadioOption list
+          Preview : Bitmap
+          InputNames : string list
+          Command : Node list -> Node
+
+        
+        }
+module Commands = 
+    do ()
+
+module Nodes =
+    module Generate =
+        do ()
+    module Transform =
+        do ()
+    module Filter =
+        do ()
